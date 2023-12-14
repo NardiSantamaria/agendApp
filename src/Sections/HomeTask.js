f5d3017641f4315b890431bdd0b0129bfd3d002d
NardiSantamaria <nardisuribel@gmail.com>
2023-12-13 21:36:39 -0600

    adding styles to home task

--- src/Sections/HomeTask.js
+++ src/Sections/HomeTask.js
@@ -3,21 +3,26 @@ import "../styles/HomeTaskStyles.scss";
 
 class HomeTask extends React.Component{
     render(){
-        return <div className="w-70">
+        return <div className="w-70 home-task">
             <div className="container-home">
-                <table>
-                    <ul>
-                        <li>
-                            task 1
-                        </li>
-                        <li>
-                            task 2
-                        </li>
-                        <li>
-                            task 3
-                        </li>
-                    </ul>
-                </table>
+                <div className="container">
+                    <table>
+                        <ul>
+                            <li>
+                                
+                            </li>
+                            <li>
+                                task 1
+                            </li>
+                            <li>
+                                task 2
+                            </li>
+                            <li>
+                                task 3
+                            </li>
+                        </ul>
+                    </table>
+                </div>
             </div>
         </div>
     }
